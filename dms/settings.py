8749f995f8bf65917c6ae91fc13a01a32bdc5634
Kamran Zulfiqar <kzulfiqar.itp@sparkcognition.local>
2022-09-19 00:22:12 +0500

    removed hard coded django secret key

--- dms/settings.py
+++ dms/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%3vum_fm25n*c%qotj*l+^9$)+5bc5wwz_r1q*dj((iyt+h_&j'
+#SECRET_KEY = 'django-insecure-%3vum_fm25n*c%qotj*l+^9$)+5bc5wwz_r1q*dj((iyt+h_&j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
