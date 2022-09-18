a58edf8f484766713329176703acfdee2f157f93
Kamran Zulfiqar <kzulfiqar.itp@sparkcognition.local>
2022-09-18 23:52:43 +0500

    automatic configuration with django-heroku

--- dms/settings.py
+++ dms/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import django_heroku
 from pathlib import Path
 
+django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
