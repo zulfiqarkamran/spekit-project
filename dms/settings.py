fcb0c9edb9a5ac69cc06cd3e3a73233fca60d152
Kamran Zulfiqar <kzulfiqar.itp@sparkcognition.local>
2022-09-19 00:36:14 +0500

    .

--- dms/settings.py
+++ dms/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import django_heroku
 from pathlib import Path
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +133,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+django_heroku.settings(locals())
