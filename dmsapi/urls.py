f40408c5246ab29034b12196df22255930621d3b
Kamran Zulfiqar <kzulfiqar.itp@sparkcognition.local>
2022-09-20 11:36:50 +0500

    added swagger

--- dmsapi/urls.py
+++ dmsapi/urls.py
@@ -1,11 +1,15 @@
 # from django.urls import include, path
 from django.urls import re_path, path
 from rest_framework import routers
+from rest_framework_swagger.views import get_swagger_view
 
 from . import views
 
 
+schema_view = get_swagger_view(title='Spekit API')
+
 urlpatterns = [
+    path('', schema_view),
     path('api/folders', views.FolderView.as_view()),
     path('api/folders/<int:id>', views.FolderDetailsView.as_view()),
     path('api/documents', views.DocumentView.as_view()),
